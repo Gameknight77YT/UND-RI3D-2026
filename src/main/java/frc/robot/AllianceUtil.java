d7a553c4de39da6e7c673cc2dbb61bec33e0eefe
Jacob Hagberg <jacobhagberg22@gmail.com>
2026-01-11 12:57:02 -0600

    Updated goal positions

--- src/main/java/frc/robot/AllianceUtil.java
+++ src/main/java/frc/robot/AllianceUtil.java
@@ -5,8 +5,8 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
 public class AllianceUtil {
-    public static final Pose2d blueGoalLoc = new Pose2d(0.0, 5.55, new Rotation2d());
-    public static final Pose2d redGoalLoc = new Pose2d(0.0, 5.55, new Rotation2d());
+    public static final Pose2d blueGoalLoc = new Pose2d(182.11, 158.84, new Rotation2d()); //Locations based on Welded perimiter found at https://firstfrc.blob.core.windows.net/frc2026/FieldAssets/2026-field-dimension-dwgs.pdf
+    public static final Pose2d redGoalLoc = new Pose2d(467.11, 158.84, new Rotation2d()); 
     public static Pose2d GetAllianceGoalPos() {
         Alliance alliance = DriverStation.getAlliance().orElse(Alliance.Blue);
 
